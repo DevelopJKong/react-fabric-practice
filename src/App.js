e05aa0eff6e5b2f6b7653610de00fbc4b6a1c9eb
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2022-06-20 17:31:35 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { fabric } from 'fabric';
 
-function App() {
+const App = () => {
+  const [canvas, setCanvas] = useState("");
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, []);
+
+  const initCanvas = () =>
+    new fabric.Canvas("canvas", {
+      height: 800,
+      width: 800,
+      backgroundColor: "pink",
+    });
+
+  const addRect = (canvi) => {
+    const rect = new fabric.Rect({
+      height: 280,
+      width: 200,
+      fill: "yellow",
+    });
+    canvi.add(rect);
+    canvi.renderAll();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <canvas id="canvas" />
+      <button onClick={() => addRect(canvas)}>Rectangle</button>
+    </>
   );
-}
+};
 
 export default App;
